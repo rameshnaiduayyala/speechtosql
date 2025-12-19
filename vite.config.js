096fb9f485cfbac5c0f748eaeec94eab48eafecb
Ramesh Ayyala <ramesh.ayyala@digitelescope.com>
2025-12-19 23:14:46 +0530

    change config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
+  server: {
+    proxy: {
+      "/": {
+        target: "http://183.82.108.168:8000",
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 });
